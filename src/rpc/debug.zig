a77b32e91524c4c6579335d02cfcf9ef5f1661d7
ch4r10t33r <psramanuj@gmail.com>
2025-10-09 10:56:59 +0100

    feat: implemented function under the debug namespace of the rpc package

--- src/rpc/debug.zig
+++ src/rpc/debug.zig
@@ -16,26 +16,65 @@ pub const DebugNamespace = struct {
 
     /// debug_traceTransaction - Returns the trace of a transaction
     pub fn traceTransaction(self: DebugNamespace, hash: Hash, options: ?TraceOptions) !TraceResult {
-        _ = self;
-        _ = hash;
-        _ = options;
-        return error.NotImplemented;
+        const hash_hex = try hash.toHex(self.client.allocator);
+        defer self.client.allocator.free(hash_hex);
+
+        var params: [2]std.json.Value = undefined;
+        params[0] = .{ .string = hash_hex };
+
+        if (options) |opts| {
+            const opts_obj = try traceOptionsToJson(self.client.allocator, opts);
+            defer opts_obj.deinit();
+            params[1] = opts_obj.value;
+
+            const result = try self.client.callWithParams("debug_traceTransaction", params[0..2]);
+            return try parseTraceResult(self.client.allocator, result);
+        } else {
+            const result = try self.client.callWithParams("debug_traceTransaction", params[0..1]);
+            return try parseTraceResult(self.client.allocator, result);
+        }
     }
 
     /// debug_traceBlockByNumber - Returns the trace of all transactions in a block
     pub fn traceBlockByNumber(self: DebugNamespace, block: types.BlockParameter, options: ?TraceOptions) ![]TraceResult {
-        _ = self;
-        _ = block;
-        _ = options;
-        return error.NotImplemented;
+        const block_param = try blockParameterToString(self.client.allocator, block);
+        defer self.client.allocator.free(block_param);
+
+        var params: [2]std.json.Value = undefined;
+        params[0] = .{ .string = block_param };
+
+        if (options) |opts| {
+            const opts_obj = try traceOptionsToJson(self.client.allocator, opts);
+            defer opts_obj.deinit();
+            params[1] = opts_obj.value;
+
+            const result = try self.client.callWithParams("debug_traceBlockByNumber", params[0..2]);
+            return try parseTraceResults(self.client.allocator, result);
+        } else {
+            const result = try self.client.callWithParams("debug_traceBlockByNumber", params[0..1]);
+            return try parseTraceResults(self.client.allocator, result);
+        }
     }
 
     /// debug_traceBlockByHash - Returns the trace of all transactions in a block
     pub fn traceBlockByHash(self: DebugNamespace, hash: Hash, options: ?TraceOptions) ![]TraceResult {
-        _ = self;
-        _ = hash;
-        _ = options;
-        return error.NotImplemented;
+        const hash_hex = try hash.toHex(self.client.allocator);
+        defer self.client.allocator.free(hash_hex);
+
+        var params: [2]std.json.Value = undefined;
+        params[0] = .{ .string = hash_hex };
+
+        if (options) |opts| {
+            const opts_obj = try traceOptionsToJson(self.client.allocator, opts);
+            defer opts_obj.deinit();
+            params[1] = opts_obj.value;
+
+            const result = try self.client.callWithParams("debug_traceBlockByHash", params[0..2]);
+            return try parseTraceResults(self.client.allocator, result);
+        } else {
+            const result = try self.client.callWithParams("debug_traceBlockByHash", params[0..1]);
+            return try parseTraceResults(self.client.allocator, result);
+        }
     }
 
     /// debug_traceCall - Executes and returns trace of a call
@@ -45,11 +84,27 @@ pub const DebugNamespace = struct {
         block: types.BlockParameter,
         options: ?TraceOptions,
     ) !TraceResult {
-        _ = self;
-        _ = call_params;
-        _ = block;
-        _ = options;
-        return error.NotImplemented;
+        const call_obj = try callParamsToJson(self.client.allocator, call_params);
+        defer call_obj.deinit();
+
+        const block_param = try blockParameterToString(self.client.allocator, block);
+        defer self.client.allocator.free(block_param);
+
+        var params: [3]std.json.Value = undefined;
+        params[0] = call_obj.value;
+        params[1] = .{ .string = block_param };
+
+        if (options) |opts| {
+            const opts_obj = try traceOptionsToJson(self.client.allocator, opts);
+            defer opts_obj.deinit();
+            params[2] = opts_obj.value;
+
+            const result = try self.client.callWithParams("debug_traceCall", params[0..3]);
+            return try parseTraceResult(self.client.allocator, result);
+        } else {
+            const result = try self.client.callWithParams("debug_traceCall", params[0..2]);
+            return try parseTraceResult(self.client.allocator, result);
+        }
     }
 
     /// debug_storageRangeAt - Returns storage range
@@ -61,13 +116,33 @@ pub const DebugNamespace = struct {
         start_key: Hash,
         limit: u64,
     ) !StorageRange {
-        _ = self;
-        _ = block_hash;
-        _ = tx_index;
-        _ = address;
-        _ = start_key;
-        _ = limit;
-        return error.NotImplemented;
+        const block_hash_hex = try block_hash.toHex(self.client.allocator);
+        defer self.client.allocator.free(block_hash_hex);
+
+        const tx_index_hex = try std.fmt.allocPrint(self.client.allocator, "0x{x}", .{tx_index});
+        defer self.client.allocator.free(tx_index_hex);
+
+        const address_hex = try address.toHex(self.client.allocator);
+        defer self.client.allocator.free(address_hex);
+
+        const start_key_hex = try start_key.toHex(self.client.allocator);
+        defer self.client.allocator.free(start_key_hex);
+
+        var params = [_]std.json.Value{
+            .{ .string = block_hash_hex },
+            .{ .integer = @intCast(tx_index) },
+            .{ .string = address_hex },
+            .{ .string = start_key_hex },
+            .{ .integer = @intCast(limit) },
+        };
+
+        const result = try self.client.callWithParams("debug_storageRangeAt", &params);
+
+        if (result != .object) {
+            return error.InvalidResponse;
+        }
+
+        return try parseStorageRange(self.client.allocator, result.object);
     }
 
     /// debug_getModifiedAccountsByNumber - Returns accounts modified in a block
@@ -76,10 +151,24 @@ pub const DebugNamespace = struct {
         start_block: u64,
         end_block: u64,
     ) ![]Address {
-        _ = self;
-        _ = start_block;
-        _ = end_block;
-        return error.NotImplemented;
+        const start_hex = try std.fmt.allocPrint(self.client.allocator, "0x{x}", .{start_block});
+        defer self.client.allocator.free(start_hex);
+
+        const end_hex = try std.fmt.allocPrint(self.client.allocator, "0x{x}", .{end_block});
+        defer self.client.allocator.free(end_hex);
+
+        var params = [_]std.json.Value{
+            .{ .string = start_hex },
+            .{ .string = end_hex },
+        };
+
+        const result = try self.client.callWithParams("debug_getModifiedAccountsByNumber", &params);
+
+        if (result != .array) {
+            return error.InvalidResponse;
+        }
+
+        return try parseAddressArray(self.client.allocator, result.array);
     }
 
     /// debug_getModifiedAccountsByHash - Returns accounts modified in a block
@@ -88,10 +177,24 @@ pub const DebugNamespace = struct {
         start_hash: Hash,
         end_hash: Hash,
     ) ![]Address {
-        _ = self;
-        _ = start_hash;
-        _ = end_hash;
-        return error.NotImplemented;
+        const start_hex = try start_hash.toHex(self.client.allocator);
+        defer self.client.allocator.free(start_hex);
+
+        const end_hex = try end_hash.toHex(self.client.allocator);
+        defer self.client.allocator.free(end_hex);
+
+        var params = [_]std.json.Value{
+            .{ .string = start_hex },
+            .{ .string = end_hex },
+        };
+
+        const result = try self.client.callWithParams("debug_getModifiedAccountsByHash", &params);
+
+        if (result != .array) {
+            return error.InvalidResponse;
+        }
+
+        return try parseAddressArray(self.client.allocator, result.array);
     }
 };
 
@@ -172,6 +275,313 @@ pub const StorageRange = struct {
     }
 };
 
+/// Helper functions
+/// JSON object wrapper for automatic cleanup
+const JsonObjectWrapper = struct {
+    value: std.json.Value,
+    allocator: std.mem.Allocator,
+
+    fn deinit(self: JsonObjectWrapper) void {
+        if (self.value == .object) {
+            self.value.object.deinit();
+        }
+    }
+};
+
+/// Convert BlockParameter to string
+fn blockParameterToString(allocator: std.mem.Allocator, block: types.BlockParameter) ![]u8 {
+    return switch (block) {
+        .latest => try allocator.dupe(u8, "latest"),
+        .earliest => try allocator.dupe(u8, "earliest"),
+        .pending => try allocator.dupe(u8, "pending"),
+        .safe => try allocator.dupe(u8, "safe"),
+        .finalized => try allocator.dupe(u8, "finalized"),
+        .number => |num| try std.fmt.allocPrint(allocator, "0x{x}", .{num}),
+    };
+}
+
+/// Convert CallParams to JSON object
+fn callParamsToJson(allocator: std.mem.Allocator, params: types.CallParams) !JsonObjectWrapper {
+    var obj = std.json.ObjectMap.init(allocator);
+
+    if (params.to) |to| {
+        const to_hex = try to.toHex(allocator);
+        try obj.put("to", .{ .string = to_hex });
+    }
+
+    if (params.from) |from| {
+        const from_hex = try from.toHex(allocator);
+        try obj.put("from", .{ .string = from_hex });
+    }
+
+    if (params.data) |data| {
+        const hex_module = @import("../utils/hex.zig");
+        const data_hex = try hex_module.bytesToHex(allocator, data);
+        try obj.put("data", .{ .string = data_hex });
+    }
+
+    if (params.value) |value| {
+        const value_hex = try value.toHex(allocator);
+        try obj.put("value", .{ .string = value_hex });
+    }
+
+    if (params.gas) |gas| {
+        const gas_hex = try std.fmt.allocPrint(allocator, "0x{x}", .{gas});
+        try obj.put("gas", .{ .string = gas_hex });
+    }
+
+    return JsonObjectWrapper{
+        .value = .{ .object = obj },
+        .allocator = allocator,
+    };
+}
+
+/// Convert TraceOptions to JSON object
+fn traceOptionsToJson(allocator: std.mem.Allocator, options: TraceOptions) !JsonObjectWrapper {
+    var obj = std.json.ObjectMap.init(allocator);
+
+    if (options.disable_storage) |val| {
+        try obj.put("disableStorage", .{ .bool = val });
+    }
+
+    if (options.disable_stack) |val| {
+        try obj.put("disableStack", .{ .bool = val });
+    }
+
+    if (options.enable_memory) |val| {
+        try obj.put("enableMemory", .{ .bool = val });
+    }
+
+    if (options.enable_return_data) |val| {
+        try obj.put("enableReturnData", .{ .bool = val });
+    }
+
+    if (options.tracer) |tracer| {
+        try obj.put("tracer", .{ .string = tracer });
+    }
+
+    if (options.timeout) |timeout| {
+        try obj.put("timeout", .{ .string = timeout });
+    }
+
+    return JsonObjectWrapper{
+        .value = .{ .object = obj },
+        .allocator = allocator,
+    };
+}
+
+/// Parse hex string to u64
+fn parseHexU64(hex_str: []const u8) !u64 {
+    const str = if (std.mem.startsWith(u8, hex_str, "0x")) hex_str[2..] else hex_str;
+    return try std.fmt.parseInt(u64, str, 16);
+}
+
+/// Parse TraceResult from JSON
+fn parseTraceResult(allocator: std.mem.Allocator, json: std.json.Value) !TraceResult {
+    if (json != .object) {
+        return error.InvalidResponse;
+    }
+
+    const obj = json.object;
+
+    // Parse gas
+    const gas_val = obj.get("gas") orelse return error.MissingField;
+    const gas = if (gas_val == .integer)
+        @as(u64, @intCast(gas_val.integer))
+    else if (gas_val == .string)
+        try parseHexU64(gas_val.string)
+    else
+        return error.InvalidFieldType;
+
+    // Parse return value
+    const return_val = obj.get("returnValue") orelse obj.get("return");
+    var return_value: []const u8 = &[_]u8{};
+    if (return_val) |rv| {
+        if (rv == .string) {
+            const hex_module = @import("../utils/hex.zig");
+            return_value = try hex_module.hexToBytes(allocator, rv.string);
+        }
+    }
+
+    // Parse struct logs
+    var struct_logs = std.ArrayList(StructLog).init(allocator);
+    defer struct_logs.deinit();
+
+    if (obj.get("structLogs")) |logs_val| {
+        if (logs_val == .array) {
+            for (logs_val.array.items) |log_val| {
+                if (log_val == .object) {
+                    const struct_log = try parseStructLog(allocator, log_val.object);
+                    try struct_logs.append(struct_log);
+                }
+            }
+        }
+    }
+
+    return TraceResult{
+        .gas = gas,
+        .return_value = return_value,
+        .struct_logs = try struct_logs.toOwnedSlice(),
+        .allocator = allocator,
+    };
+}
+
+/// Parse array of TraceResults
+fn parseTraceResults(allocator: std.mem.Allocator, json: std.json.Value) ![]TraceResult {
+    if (json != .array) {
+        return error.InvalidResponse;
+    }
+
+    var results = std.ArrayList(TraceResult).init(allocator);
+    defer results.deinit();
+
+    for (json.array.items) |item| {
+        const trace = try parseTraceResult(allocator, item);
+        try results.append(trace);
+    }
+
+    return try results.toOwnedSlice();
+}
+
+/// Parse StructLog from JSON
+fn parseStructLog(allocator: std.mem.Allocator, obj: std.json.ObjectMap) !StructLog {
+    const pc_val = obj.get("pc") orelse return error.MissingField;
+    const pc = if (pc_val == .integer)
+        @as(u64, @intCast(pc_val.integer))
+    else
+        return error.InvalidFieldType;
+
+    const op_val = obj.get("op") orelse return error.MissingField;
+    const op = if (op_val == .string)
+        op_val.string
+    else
+        return error.InvalidFieldType;
+
+    const gas_val = obj.get("gas") orelse return error.MissingField;
+    const gas = if (gas_val == .integer)
+        @as(u64, @intCast(gas_val.integer))
+    else
+        return error.InvalidFieldType;
+
+    const gas_cost_val = obj.get("gasCost") orelse return error.MissingField;
+    const gas_cost = if (gas_cost_val == .integer)
+        @as(u64, @intCast(gas_cost_val.integer))
+    else
+        return error.InvalidFieldType;
+
+    const depth_val = obj.get("depth") orelse return error.MissingField;
+    const depth = if (depth_val == .integer)
+        @as(u64, @intCast(depth_val.integer))
+    else
+        return error.InvalidFieldType;
+
+    // Optional fields
+    var stack: ?[]U256 = null;
+    if (obj.get("stack")) |stack_val| {
+        if (stack_val == .array) {
+            var stack_items = std.ArrayList(U256).init(allocator);
+            defer stack_items.deinit();
+
+            for (stack_val.array.items) |item| {
+                if (item == .string) {
+                    const value = try U256.fromHex(item.string);
+                    try stack_items.append(value);
+                }
+            }
+
+            stack = try stack_items.toOwnedSlice();
+        }
+    }
+
+    var memory: ?[]const u8 = null;
+    if (obj.get("memory")) |mem_val| {
+        if (mem_val == .array) {
+            // Memory is returned as array of hex strings
+            var mem_data = std.ArrayList(u8).init(allocator);
+            defer mem_data.deinit();
+
+            for (mem_val.array.items) |item| {
+                if (item == .string) {
+                    const hex_module = @import("../utils/hex.zig");
+                    const bytes = try hex_module.hexToBytes(allocator, item.string);
+                    defer allocator.free(bytes);
+                    try mem_data.appendSlice(bytes);
+                }
+            }
+
+            memory = try mem_data.toOwnedSlice();
+        }
+    }
+
+    return StructLog{
+        .pc = pc,
+        .op = op,
+        .gas = gas,
+        .gas_cost = gas_cost,
+        .depth = depth,
+        .stack = stack,
+        .memory = memory,
+        .storage = null, // Storage parsing is complex, can be added if needed
+        .allocator = allocator,
+    };
+}
+
+/// Parse StorageRange from JSON
+fn parseStorageRange(allocator: std.mem.Allocator, obj: std.json.ObjectMap) !StorageRange {
+    var storage = std.StringHashMap(StorageRange.StorageEntry).init(allocator);
+
+    if (obj.get("storage")) |storage_val| {
+        if (storage_val == .object) {
+            var it = storage_val.object.iterator();
+            while (it.next()) |entry| {
+                if (entry.value_ptr.* == .object) {
+                    const key_val = entry.value_ptr.object.get("key");
+                    const value_val = entry.value_ptr.object.get("value");
+
+                    if (key_val != null and value_val != null) {
+                        if (key_val.? == .string and value_val.? == .string) {
+                            const key = try Hash.fromHex(key_val.?.string);
+                            const value = try Hash.fromHex(value_val.?.string);
+
+                            const entry_key = try allocator.dupe(u8, entry.key_ptr.*);
+                            try storage.put(entry_key, .{ .key = key, .value = value });
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    var next_key: ?Hash = null;
+    if (obj.get("nextKey")) |next_val| {
+        if (next_val == .string) {
+            next_key = try Hash.fromHex(next_val.string);
+        }
+    }
+
+    return StorageRange{
+        .storage = storage,
+        .next_key = next_key,
+        .allocator = allocator,
+    };
+}
+
+/// Parse address array from JSON
+fn parseAddressArray(allocator: std.mem.Allocator, array: std.json.Array) ![]Address {
+    var addresses = std.ArrayList(Address).init(allocator);
+    defer addresses.deinit();
+
+    for (array.items) |item| {
+        if (item != .string) {
+            return error.InvalidFieldType;
+        }
+        const addr = try Address.fromHex(item.string);
+        try addresses.append(addr);
+    }
+
+    return try addresses.toOwnedSlice();
+}
+
 test "debug namespace creation" {
     const allocator = std.testing.allocator;
 
@@ -186,4 +596,29 @@ test "trace options default" {
     const options = TraceOptions{};
     try std.testing.expect(options.disable_storage == null);
     try std.testing.expect(options.disable_stack == null);
+}
+
+test "trace options to json" {
+    const allocator = std.testing.allocator;
+
+    const options = TraceOptions{
+        .disable_storage = true,
+        .enable_memory = true,
+        .timeout = "5s",
+    };
+
+    const json_obj = try traceOptionsToJson(allocator, options);
+    defer json_obj.deinit();
+
+    try std.testing.expect(json_obj.value == .object);
+    try std.testing.expect(json_obj.value.object.contains("disableStorage"));
+    try std.testing.expect(json_obj.value.object.contains("enableMemory"));
+}
+
+test "parse hex u64" {
+    const value1 = try parseHexU64("0x10");
+    try std.testing.expectEqual(@as(u64, 16), value1);
+
+    const value2 = try parseHexU64("ff");
+    try std.testing.expectEqual(@as(u64, 255), value2);
 }
